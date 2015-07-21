4ba30d2c8b8d965325f54b3fffc55f0aeb2f5834
glassysky <873661157@qq.com>
2015-07-21 20:21:10 +0800

    change img

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# glassysky.github.io
+﻿# glassysky.github.io
 待改<br>
 1.倒计时<br>
 2.重新开始<br>
 3.最高分<br>
 4.floor 和 人物<br>
-5.
+5.血条<br>
+6.不同精灵球不同速度不同加分<br>
--- game/src/MainMenu.js
+++ game/src/MainMenu.js
@@ -3,7 +3,7 @@ Ball.MainMenu.prototype = {
 	create: function(){
 		// 显示图片
 		this.add.sprite(0, 0, 'background');
-		this.add.sprite(-130, Ball.GAME_HEIGHT-514, 'monster-cover');
+		this.add.sprite(0, Ball.GAME_HEIGHT-514, 'monster-cover');
 		this.add.sprite((Ball.GAME_WIDTH-395)/2, 60, 'title');
 		// 增加开始按钮
 		this.add.button(Ball.GAME_WIDTH-401-10, Ball.GAME_HEIGHT-143-10, 'button-start', this.startGame, this, 1, 0, 2);
